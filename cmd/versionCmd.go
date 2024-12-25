56a84fd73c62fc640d60895aca70d4396dfba02a
Aryan Sharma <sharmaaryan9837@gmail.com>
2024-12-25 16:05:02 +0000

    Updated versionCmd file

--- cmd/versionCmd.go
+++ cmd/versionCmd.go
@@ -5,3 +5,19 @@ import (
 
 	"github.com/spf13/cobra"
 )
+
+const CliVersion = "v1.5.0"
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Know the installed version of gencli",
+	Long:  `This command will help you to know the installed version of gencli`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("gencli version:", CliVersion)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
